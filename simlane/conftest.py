aef9bd14caaf086c58446cfce455b88d1012cb9d
Akash Vibhute <21662960+akashvibhute@users.noreply.github.com>
2025-06-16 08:34:18 +0100

    initial commit

---
+++ simlane/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from simlane.users.models import User
+from simlane.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
